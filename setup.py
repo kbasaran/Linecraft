7f9b8a15e763a5490f6e65354ad62421c933aa49
kbasaran <kbasaran@gmail.com>
2023-09-23 21:03:59 +0200

    make setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import sys
+from cx_Freeze import setup, Executable
+
+# Dependencies are automatically detected, but it might need fine tuning.
+build_exe_options = {
+    "packages": ["scipy", "numpy"],
+    "include_files": [
+        (".\LICENSE", ".\LICENSE"),
+        (".\README.md", ".\README.md"),
+        ],
+    "silent_level": 1,
+}
+
+# base="Win32GUI" should be used only for Windows GUI app
+base = "Win32GUI" if sys.platform == "win32" else None
+
+setup(
+    name="Linecraft",
+    version="0.1.0",
+    description="Frequency response plotting and statistics",
+    options={"build_exe": build_exe_options},
+    executables=[Executable("main.pyw", base=base)],
+)
