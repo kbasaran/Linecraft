020b3f81991725f0163cb51ffccee2701fe6f500
kbasaran <kbasaran@gmail.com>
2023-09-24 10:27:44 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -18,15 +18,20 @@ build_exe_options = {
 # base="Win32GUI" should be used only for Windows GUI app
 base = "Win32GUI" if sys.platform == "win32" else None
 
+executables=[Executable("main.pyw",
+                        copyright="Copyright (C) 2023 Kerem Basaran",
+                        base=base,
+                        shortcut_name="Linecraft 0.1.0",
+                        shortcut_dir="DesktopFolder",
+                        icon=r".\logo\icon.ico",
+                        )
+            ],
+
 setup(
     name="Linecraft",
     version="0.1.0",
     description="Frequency response plotting and statistics",
     options={"build_exe": build_exe_options},
-    executables=[Executable("main.pyw",
-                            base=base,
-                            shortcut_name="Linecraft 0.1.0",
-                            shortcut_dir="DesktopFolder",
-                            icon=r".\logo\icon.ico",
-                            )],
+    executables=executables,
 )
+
