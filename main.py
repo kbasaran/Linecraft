7e644813262100e66dd6546383d1ce66f3255960
kbasaran <kbasaran@proton.me>
2025-01-22 10:33:37 +0100

    handle tight layout in main

--- main.py
+++ main.py
@@ -246,7 +246,10 @@ class CurveAnalyze(qtw.QMainWindow):
 
     def _create_widgets(self):
         # ---- Create graph and buttons widget
-        self.graph = MatplotlibWidget(settings)
+        self.graph = MatplotlibWidget(settings, layout_engine="tight")
+        # mpl.pyplot.rcParams["figure.constrained_layout.h_pad"] = 0.3
+        # mpl.pyplot.rcParams["figure.constrained_layout.w_pad"] = 0.4
+
         self.graph.set_y_limits_policy("SPL")
         self.graph_buttons = pwi.PushButtonGroup(
             {
@@ -1289,6 +1292,8 @@ class ProcessingDialog(qtw.QDialog):
         self.setWindowTitle("Processing Menu")
         layout = qtw.QVBoxLayout(self)
         self.tab_widget = qtw.QTabWidget()
+        text_width = qtg.QFontMetrics(self.font()).averageCharWidth()
+        self.tab_widget.setMinimumWidth(text_width * 100)
         layout.addWidget(self.tab_widget)
 
         # dict of tuples. key is index of tab. value is tuple with (UserForm, name of function to use for its calculation)
